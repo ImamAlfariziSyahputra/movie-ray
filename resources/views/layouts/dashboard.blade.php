e3bc6022785c062e5925582adcc6a46b12cab1ef
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-17 09:09:16 +0700

    setup admin template

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>{{ config('app.name') }}</title>
+        <link href="{{ asset('sbadmin/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <!-- Navbar -->
+        @include('layouts._dashboard.navbar')
+        <!-- End Navbar -->
+        <div id="layoutSidenav">
+            <!-- Sidebar Nav -->
+            @include('layouts._dashboard.sidebar')
+            <!-- End Sidebar Nav -->
+            <div id="layoutSidenav_content">
+                <main>
+                    @yield('content')
+                </main>
+                @include('layouts._dashboard.footer')
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('sbadmin/js/scripts.js') }}"></script>
+    </body>
+</html>
